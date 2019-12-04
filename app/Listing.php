d4bbffef5ac026d377823a0b1ca7b54ca7d4c0ad
ahurm <57002950+ahurm@users.noreply.github.com>
2019-12-04 23:19:47 +0200

    Update Listing.php

--- app/Listing.php
+++ app/Listing.php
@@ -9,6 +9,7 @@ class Listing extends Model
     public function user() {
         
         //One to Many relationship with User model
+        //Using id as user_id "foreign key"
         return $this->belongsTo('App\User');
     }
 }
