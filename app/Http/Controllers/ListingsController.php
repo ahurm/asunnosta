6919fb90f6b8fabfc0eced9353e1b92c5f314f23
ahurm <57002950+ahurm@users.noreply.github.com>
2019-12-04 22:48:31 +0200

    Update ListingsController.php

--- app/Http/Controllers/ListingsController.php
+++ app/Http/Controllers/ListingsController.php
@@ -2,108 +2,237 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use App\Listing;
+use Illuminate\Support\Facades\Cookie;
 
-class SearchController extends Controller
+class ListingsController extends Controller
 {
-    public function search(Request $request)
-    {
-        //Variables for sorting
-        $sort = 'asc';
-        $prop = 'created_at';
-        
-        //Read sorting parameter from request
-        $sortby = $request->input('sortby');
-        
-        //Read filter parameters from request
-        $types = $request->input('types');
-        $rooms = $request->input('rooms');
-        $priceMin = (int) $request->input('priceMin');
-        $priceMax = (int) $request->input('priceMax');
-        $sizeMin = (int) $request->input('sizeMin');
-        $sizeMax = (int) $request->input('sizeMax');
 
-        //Check sorting method
-        switch ($sortby) {
-            case 'Recent':
-                $sort = 'desc';
-                $prop = 'created_at';
-            break;
-            case 'Oldest':
-                $sort = 'asc';
-                $prop = 'created_at';
-            break;
-            case 'Price ascending':
-                $sort = 'asc';
-                $prop = 'price';
-            break;
-            case 'Price descending':
-                $sort = 'desc';
-                $prop = 'price';
-            break;
-            case 'Size ascending':
-                $sort = 'asc';
-                $prop = 'size';
-            break;
-            case 'Size descending':
-                $sort = 'desc';
-                $prop = 'size';
-            break;
-            default: 
-            $sort = 'asc';
-            $prop = 'created_at';
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $listings = Listing::orderBy('created_at', 'desc')->get();
+        return view('listings.index')->with('listings', $listings);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('listings.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'image' => 'image|nullable|max:1999',
+            'address' => 'required',
+            'bedrooms' => 'required',
+            'bathrooms' => 'required',
+            'size' => 'required|digits_between:0,9',
+            'type' => 'required',
+            'price' => 'required|digits_between:0,9',
+        ]);
+
+        // Price to integer
+        $priceInt = (int)$request->input('price');
+
+        // OpenCage Geocoding API
+        $geocoder = new \OpenCage\Geocoder\Geocoder('8582bc40ea1147dfa3e558676a10d122');
+        $result = $geocoder->geocode($request->input('address'), ['language' => 'fi', 'countrycode' => 'fi']);
+        if ($result && $result['total_results'] > 0) {
+          $first = $result['results'][0];
+          # 4.360081;43.8316276;6 Rue Massillon, 30020 Nîmes, Frankreich
+          $lng = $first['geometry']['lng'];
+          $lat = $first['geometry']['lat'];
+          
         }
 
+        if($request->hasFile('image')){
 
-        //Query builder, using functions to resolve used filters
-        $listings = Listing::
-        where(function ($query) use ($types) {
+            $filenameWithExt = $request->file('image')->getClientOriginalName();
 
-            if ($types != null) {
-                $query->whereIn('type', $types);
-            } else {
-                $query->whereNotNull('type');
-            }
-        })
-        ->where(function ($query) use ($rooms) {
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            $extension = $request->file('image')->getClientOriginalExtension();
+            $fileNameToStore = $filename.'_'.time().'.'.$extension;
+            $path = $request->file('image')->storeAs('public/images', $fileNameToStore);
 
-            if ($rooms != null) {
-                $query->whereIn('bedrooms', $rooms);
-            } else {
-                $query->whereNotNull('bedrooms');
-            }
-        })
-        ->where(function ($query) use ($priceMin, $priceMax) {
-            if ($priceMin != null && $priceMax == null) {
-                $query->where('price', '>=', $priceMin);
-            }
-            if ($priceMin == null && $priceMax != null) {
-                $query->where('price', '<=', $priceMax);
-            }
-            if ($priceMin != null && $priceMax != null) {
-                $query->whereBetween('price', [$priceMin, $priceMax]);
-            }
-        })
-        ->where(function ($query) use ($sizeMin, $sizeMax) {
-            if ($sizeMin != null && $sizeMax == null) {
-                $query->where('size', '>=', $sizeMin);
-            }
-            if ($sizeMin == null && $sizeMax != null) {
-                $query->where('size', '<=', $sizeMax);
-            }
-            if ($sizeMin != null && $sizeMax != null) {
-                $query->whereBetween('size', [$sizeMin, $sizeMax]);
-            }
-        })
-        ->orderBy($prop, $sort) //Using sorting method which was checked before
+        } else {
 
-        ->get();
+           $fileNameToStore = "noimage.jpg";
+        }
 
+        $listing = new Listing;
+        $listing->user_id = auth()->user()->id;
+        $listing->image = $fileNameToStore;
+        $listing->address = $request->input('address');
+        $listing->bedrooms = $request->input('bedrooms');
+        $listing->bathrooms = $request->input('bathrooms');
+        $listing->size = $request->input('size');
+        $listing->type = $request->input('type');
+        $listing->desc = $request->input('desc');
+        $listing->price = $priceInt;
+        $listing->latitude = $lng;
+        $listing->longtitude = $lat;
+        $listing->save();
 
+        return redirect('/listings')->with('success', 'Listing created');
+    }
 
-        //Return filtered and sorted listings
-        return view('listings.index')->with('listings', $listings);
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+/*         return Storage::get('public/images/kerrostalo8_1575377423.jpg'); */
+        $minutes = 10080; // Week
+         if(Cookie::has('visited')) {
+            $visited = json_decode(Cookie::get('visited'));
+        } else {
+            $visited = [];
+        } 
+
+        array_push($visited, $id);
+        $visited = array_slice(array_unique($visited), -3);
+
+        Cookie::queue(Cookie::make('visited', json_encode($visited), $minutes)); 
+        $listing = Listing::find($id);
+        
+
+        return view('listings.show')->with('listing', $listing);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $listing = Listing::find($id);
+
+        if(auth()->user()->id !== $listing->user_id) {
+            return redirect('/listings')->with('error', 'Unauthorized page'); 
+        }
+
+        return view('listings.edit')->with('listing', $listing);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'image' => 'image|nullable|max:1999',
+            'address' => 'required',
+            'bedrooms' => 'required',
+            'bathrooms' => 'required',
+            'size' => 'required|digits_between:0,9',
+            'type' => 'required',
+            'price' => 'required|digits_between:0,9',
+        ]);
+
+        // Price to integer
+        $priceInt = (int)$request->input('price');
+
+        // OpenCage Geocoding API
+        $geocoder = new \OpenCage\Geocoder\Geocoder('8582bc40ea1147dfa3e558676a10d122');
+        $result = $geocoder->geocode($request->input('address'), ['language' => 'fi', 'countrycode' => 'fi']);
+        if ($result && $result['total_results'] > 0) {
+          $first = $result['results'][0];
+          # 4.360081;43.8316276;6 Rue Massillon, 30020 Nîmes, Frankreich
+          $lng = $first['geometry']['lng'];
+          $lat = $first['geometry']['lat'];
+          
+        }
+
+        if($request->hasFile('image')){
+
+            $filenameWithExt = $request->file('image')->getClientOriginalName();
+
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            $extension = $request->file('image')->getClientOriginalExtension();
+            $fileNameToStore = $filename.'_'.time().'.'.$extension;
+            $path = $request->file('image')->storeAs('public/images', $fileNameToStore);
+
+        } else {
+
+           $fileNameToStore = "noimage.jpg";
+        }
+
+        $listing = Listing::find($id);
+        $listing->user_id = auth()->user()->id;
+        $listing->image = $fileNameToStore;
+        $listing->address = $request->input('address');
+        $listing->bedrooms = $request->input('bedrooms');
+        $listing->bathrooms = $request->input('bathrooms');
+        $listing->size = $request->input('size');
+        $listing->type = $request->input('type');
+        $listing->desc = $request->input('desc');
+        $listing->price = $priceInt;
+        $listing->latitude = $lng;
+        $listing->longtitude = $lat;
+        $listing->save();
+
+        return redirect('/listings')->with('success', 'Listing updated');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $listing = Listing::find($id);
+
+        if(auth()->user()->id !== $listing->user_id) {
+            return redirect('/listings')->with('error', 'Unauthorized page'); 
+        }
+
+        if($listing->image != 'noimage.jpg'){
+            Storage::delete('public/images/'.$listing->image);
+        }
+
+        $listing->delete();
+        return redirect('/listings')->with('success', 'Listing Removed');
     }
 }
